3b4a4d6b4251c93fe4091770bcbd9e84ee0f790f
Dongeun Lee <sixgen@gmail.com>
2023-07-29 16:12:51 +0900

    v0.8.0 upgrade sha256 hash hex base64

--- src/sha256-mbp.js
+++ src/sha256-mbp.js
@@ -1,12 +1,27 @@
-import { hash as sha256, hmac } from '../lib/fast-sha256.js'
+import { hash, hmac } from '../lib/fast-sha256.js'
 import { MBP } from 'meta-buffer-pack'
 
+/*
+Tip.
+ fast-sha256 use data:8Uint8Array
+ sha256-mbp use data:any  ( internal type converter )
+
+ MBP.U8( any ) return Uint8Array
+ MBP.B8( any ) reutn Buffer instance
+
+*/
+const sha256 = {};
+
 sha256.hash = function (data) {
-  return sha256(MBP.U8(data))
+  return hash(MBP.U8(data))
 }
 
 sha256.hex = function (data) {
-  return MBP.hex(sha256.hash(data))
+  return MBP.B8( hash(MBP.U8(data)) ).toString('hex')
+}
+
+sha256.base64= function (data) {
+  return MBP.B8(  hash(MBP.U8(data)) ).toString('base64')
 }
 
 sha256.hmac = function (key, data) {
